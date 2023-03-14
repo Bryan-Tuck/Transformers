06d134cb0149eaff491c53d7da8044fe0e411a91
Bryan-Tuck <83679976+Bryan-Tuck@users.noreply.github.com>
2023-03-14 15:18:45 -0500

    Update run.py
    Slight refactoring. Introducing load_data, get_data_loaders, and init_trainer_and_model in run.py

--- run.py
+++ run.py
@@ -33,64 +33,83 @@ LEARNING_RATE = 2e-5  # 2e-5 is the default learning rate for BERT
 N_CLASSES = 2  # Number of classes in the dataset
 DIRECTORY = 'olid'  # Directory where the csv files are stored
 
-# Main function to run the training and evaluation of the model.
+def load_data(file_path):
+    """
+    Load the dataset from the given file path and split it into train, validation, and test sets.
+    
+    :param file_path: str, path to the CSV file containing the dataset
+    :return: tuple, (train, val, test) DataFrames
+    """
+    data = pd.read_csv(file_path, usecols=['tweet_clean', 'label'])
+    train, val = train_test_split(data, test_size=0.2, random_state=RANDOM_SEED)
+    val, test = train_test_split(val, test_size=0.5, random_state=RANDOM_SEED)
 
+    return train, val, test
+
+def get_data_loaders(train, val, test):
+    """
+    Create data loaders for train, validation, and test sets.
+
+    :param train: DataFrame, the training set
+    :param val: DataFrame, the validation set
+    :param test: DataFrame, the test set
+    :return: tuple, (train_data_loader, val_data_loader, test_data_loader) PyTorch DataLoader objects
+    """
+    train_tweets = train.tweet_clean.values
+    train_labels = train.label.values
+    val_tweets = val.tweet_clean.values
+    val_labels = val.label.values
+    test_tweets = test.tweet_clean.values
+    test_labels = test.label.values
+
+    train_data_loader = get_data_loader(train_tweets, train_labels, TOKENIZER, BATCH_SIZE)
+    val_data_loader = get_data_loader(val_tweets, val_labels, TOKENIZER, BATCH_SIZE)
+    test_data_loader = get_data_loader(test_tweets, test_labels, TOKENIZER, BATCH_SIZE)
+
+    return train_data_loader, val_data_loader, test_data_loader
+
+
+def init_trainer_and_model(train_data_loader, val_data_loader, test_data_loader):
+    """
+    Initialize the model, trainer, criterion, and optimizer.
+
+    :param train_data_loader: DataLoader, the training data loader
+    :param val_data_loader: DataLoader, the validation data loader
+    :param test_data_loader: DataLoader, the test data loader
+    :return: tuple, (trainer, model) Trainer and TweetClassifier objects
+    """
+    model = TweetClassifier(MODEL_TYPE, n_classes=N_CLASSES)
+    model = model.to(DEVICE)
+    optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+    criterion = nn.CrossEntropyLoss().to(DEVICE)
+
+    trainer = Trainer(model, train_data_loader, val_data_loader,
+                      test_data_loader, criterion, optimizer, DEVICE)
+
+    return trainer, model
 
 def main():
-    directory = DIRECTORY  # directory where the csv files are stored
-    start_time = time.time()  # start time of the program
-    for file in os.listdir(directory):  # loop over the files in the directory
-        if file.endswith('.csv'):  # check if the file is a csv file
-            file_path = os.path.join(directory, file)  # get the file path
-            # get the method name from the file name
+    start_time = time.time()
+
+    for file in os.listdir(DIRECTORY):
+        if file.endswith('.csv'):
+            file_path = os.path.join(DIRECTORY, file)
             method = get_method_name_from_file(file)
             print(f'Running method {method}')
 
             # Initialize lists to store the scores for each run
-            precision_macro_scores = []
-            recall_macro_scores = []
-            f1_macro_scores = []
-
-            precision_weighted_scores = []
-            recall_weighted_scores = []
-            f1_weighted_scores = []
-            accuracy_scores = []
-            mcc_scores = []
+            (precision_macro_scores, recall_macro_scores, f1_macro_scores,
+             precision_weighted_scores, recall_weighted_scores, f1_weighted_scores,
+             accuracy_scores, mcc_scores) = ([] for _ in range(8))
 
             # Load your data
-            data = pd.read_csv(file_path, usecols=['tweet_clean', 'label'])
-            # Split your data into train, validation and test sets
-            train, val = train_test_split(
-                data, test_size=0.2, random_state=RANDOM_SEED)
-            val, test = train_test_split(
-                val, test_size=0.5, random_state=RANDOM_SEED)
+            train, val, test = load_data(file_path)
 
-            # Get the tweets and labels
-            train_tweets = train.tweet_clean.values
-            train_labels = train.label.values
-            val_tweets = val.tweet_clean.values
-            val_labels = val.label.values
-            test_tweets = test.tweet_clean.values
-            test_labels = test.label.values
+            # Get the data loaders
+            train_data_loader, val_data_loader, test_data_loader = get_data_loaders(train, val, test)
 
-            # Load your data
-            train_data_loader = get_data_loader(
-                train_tweets, train_labels, TOKENIZER, BATCH_SIZE)
-            val_data_loader = get_data_loader(
-                val_tweets, val_labels, TOKENIZER, BATCH_SIZE)
-            test_data_loader = get_data_loader(
-                test_tweets, test_labels, TOKENIZER, BATCH_SIZE)
-
-            # Define your model, criterion, optimizer and device
-            model = TweetClassifier(MODEL_TYPE, n_classes=N_CLASSES)
-            model = model.to(DEVICE)
-            optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
-            # CrossEntropyLoss is used for multi-class classification
-            criterion = nn.CrossEntropyLoss().to(DEVICE)
-
-            # Initialize the trainer
-            trainer = Trainer(model, train_data_loader, val_data_loader,
-                              test_data_loader, criterion, optimizer, DEVICE)
+            # Initialize the trainer and model
+            trainer, model = init_trainer_and_model(train_data_loader, val_data_loader, test_data_loader)
 
             # Train your model
             trainer.train(num_epochs=EPOCHS, use_scheduler=True)
@@ -109,8 +128,8 @@ def main():
             y_test, y_pred = trainer.test()
 
             # Generate evaluation report
-            precision_macro, recall_macro, f1_macro, precision_weighted, recall_weighted, f1_weighted, accuracy_score, matthews_correlation_coefficient = get_classification_report(
-                y_test, y_pred)
+            (precision_macro, recall_macro, f1_macro, precision_weighted,
+             recall_weighted, f1_weighted, accuracy_score, matthews_correlation_coefficient) = get_classification_report(y_test, y_pred)
 
             # Store the scores
             precision_macro_scores.append(precision_macro)
@@ -130,15 +149,14 @@ def main():
 
             # Save the model
             save_path = f'saved_models\method_{method}.pth'
-            torch.save(model.state_dict(), save_path)  # save the model
-            torch.cuda.empty_cache()  # clear the cache
+            torch.save(model.state_dict(), save_path)
+            torch.cuda.empty_cache()
 
-    end_time = time.time()  # end time of the program
-    total_time = end_time - start_time  # total running time of the program
-    # print the total running time
+    end_time = time.time()
+    total_time = end_time - start_time
     print("Total running time:", total_time, "seconds")
 
 
 if __name__ == '__main__':
-    """ Run the main function. """
     main()
+
